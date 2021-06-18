5940a109640084be4894f9eb8eae20e698d84fe4
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-06-19 03:11:10 +0800

    optimize generation algorithm; reload page instead of rescale canvas when resize;

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ const G = 1
 const B = 2
 const A = 3
 const FULL = 255
-const PIXEL = 8
-const targetFPS = 3
+const PIXEL = 1
+const targetFPS = 15
 const targetInterval = 1000 / targetFPS
-const { random, floor, min } = Math
+const { random, floor, min, abs } = Math
 const { MAX_SAFE_INTEGER } = Number
 
 function toIndex(x: number, y: number, W: number) {
@@ -21,31 +21,20 @@ function fromIndex(offset: offset, W: number) {
 }
 
 const canvas = document.querySelector('canvas#paint') as HTMLCanvasElement
+let W = floor(window.innerWidth / PIXEL)
+let H = floor(window.innerHeight / PIXEL)
+canvas.width = W
+canvas.height = H
 const context = canvas.getContext('2d')
-let W: number = 32
-let H: number = 32
 let image = context.createImageData(W, H)
+let stopped = false
 function resize() {
-  W = floor(window.innerWidth / PIXEL)
-  H = floor(window.innerHeight / PIXEL)
-  canvas.width = W
-  canvas.height = H
-  const newImage = context.createImageData(W, H)
-  for (let y = 0; y < H; y++) {
-    const oldY = floor((y / H) * image.height)
-    for (let x = 0; x < W; x++) {
-      const oldX = floor((x / W) * image.width)
-      const newI = toIndex(x, y, W)
-      const oldI = toIndex(oldX, oldY, image.width)
-      newImage.data[newI + G] = image.data[oldI + G]
-      newImage.data[newI + B] = image.data[oldI + B]
-      newImage.data[newI + A] = image.data[oldI + A]
-    }
-  }
-  image = newImage
+  console.log('resize')
+  stopped = true
+  location.reload()
+  setTimeout(() => location.reload())
 }
 window.addEventListener('resize', resize)
-resize()
 
 type offset = number
 type Edge = offset
@@ -64,6 +53,15 @@ function nextColor() {
   }
 }
 
+function checkEmptyEdge(edgeX: number, edgeY: number) {
+  if (edgeY === -1 || edgeY === H) return
+  if (edgeX === -1 || edgeX === W) return
+  const edgeOffset = toIndex(edgeX, edgeY, W)
+  if (image.data[edgeOffset + A] !== FULL && !edges.includes(edgeOffset)) {
+    edges.push(edgeOffset)
+  }
+}
+
 function place(
   r: number,
   g: number,
@@ -72,23 +70,14 @@ function place(
   y: number,
   offset: number,
 ) {
-  for (;;) {
-    image.data[offset + R] = r
-    image.data[offset + G] = g
-    image.data[offset + B] = b
-    image.data[offset + A] = FULL
-    for (const dy of [-1, 0, 1]) {
-      const edgeY = y + dy
-      for (const dx of [-1, 0, 1]) {
-        const edgeX = x + dx
-        const edgeOffset = toIndex(edgeX, edgeY, W)
-        if (image.data[edgeOffset + A] !== FULL) {
-          edges.push(edgeOffset)
-        }
-      }
-    }
-    return
-  }
+  image.data[offset + R] = r
+  image.data[offset + G] = g
+  image.data[offset + B] = b
+  image.data[offset + A] = FULL
+  checkEmptyEdge(x, y - 1)
+  checkEmptyEdge(x, y + 1)
+  checkEmptyEdge(x - 1, y)
+  checkEmptyEdge(x + 1, y)
 }
 function init() {
   const { r, g, b } = nextColor()
@@ -100,37 +89,42 @@ function init() {
 init()
 
 function iterate() {
+  if (stopped) return
   const { r, g, b } = nextColor()
-  let minEdgeD2 = MAX_SAFE_INTEGER
+  let minEdgeDiff = MAX_SAFE_INTEGER
   let minOffset = 0
   let minX = 0
   let minY = 0
   let minEdgeIndex = 0
   edges.forEach((offset, edgeIndex) => {
     const { x, y } = fromIndex(offset, W)
-    for (const dy of [-1, 0, 1]) {
-      const edgeY = y + dy
-      for (const dx of [-1, 0, 1]) {
-        if (dy === 0 && dx === 0) continue
-        const edgeX = x + dx
-        const edgeOffset = toIndex(edgeX, edgeY, W)
-        if (image.data[edgeOffset + A] === FULL) {
-          const dr = image.data[edgeOffset + R] - r
-          const dg = image.data[edgeOffset + G] - g
-          const db = image.data[edgeOffset + B] - b
-          const d2 = dr * dr + dg * dg + db * db
-          if (d2 < minEdgeD2) {
-            minEdgeD2 = d2
-            minOffset = offset
-            minX = x
-            minY = y
-            minEdgeIndex = edgeIndex
-          }
-        }
+    let diff = 0
+    let nEdge = 0
+    function checkEdgeDiff(edgeX: number, edgeY: number) {
+      const edgeOffset = toIndex(edgeX, edgeY, W)
+      if (image.data[edgeOffset + A] === FULL) {
+        nEdge++
+        const dr = image.data[edgeOffset + R] - r
+        const dg = image.data[edgeOffset + G] - g
+        const db = image.data[edgeOffset + B] - b
+        diff += abs(dr) + abs(dg) + abs(db)
+        // diff += dr * dr + dg * dg + db * db
       }
     }
+    checkEdgeDiff(x, y - 1)
+    checkEdgeDiff(x, y + 1)
+    checkEdgeDiff(x - 1, y)
+    checkEdgeDiff(x + 1, y)
+    diff /= nEdge
+    if (diff && diff < minEdgeDiff) {
+      minEdgeDiff = diff
+      minOffset = offset
+      minX = x
+      minY = y
+      minEdgeIndex = edgeIndex
+    }
   })
-  if (minEdgeD2 != MAX_SAFE_INTEGER) {
+  if (minEdgeDiff != MAX_SAFE_INTEGER) {
     edges.splice(minEdgeIndex, 1)
     place(r, g, b, minX, minY, minOffset)
   }
@@ -139,6 +133,10 @@ function iterate() {
 let lastTime = Date.now()
 let batchSize = 1000
 function draw() {
+  if (stopped) {
+    console.log('stopped')
+    return
+  }
   if (edges.length === 0) {
     console.log('no edges')
     return
