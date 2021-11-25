f442fabe6d0e5af08e5b746adaa9b58aa01f5955
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-11-25 20:30:43 +0800

    making CPU-like shape

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
-document.title = 'all rgb'
+const { round, floor, ceil, random, min, max } = Math
 
-const style: HTMLStyleElement = document.createElement('style')
-style.textContent = `
-body {
-  padding: 0;
-  margin: 0;
-  overflow: hidden;
-}
-`
-document.body.appendChild(style)
-
-const canvas: HTMLCanvasElement = document.createElement('canvas')
-document.body.appendChild(canvas)
-const w = (canvas.width = 400 * 0 || screen.availWidth)
-const h = (canvas.height = 400 * 0 || screen.availHeight)
+const canvas: HTMLCanvasElement = document.querySelector('canvas#allrgb')
+const rect = canvas.getBoundingClientRect()
+const w = floor(rect.width)
+const h = floor(rect.height)
+canvas.width = w
+canvas.height = h
+console.log({ w, h })
 const context = canvas.getContext('2d')
 const imageData = context.createImageData(w, h)
+
+const n_trial = 32
+const range = 5
+const range2 = range * range
+const fps = 30
+const maxBatch = 64
+const min_peer = 2
+
+const timePerFrame = 1000 / fps
 
 function render() {
   context.putImageData(imageData, 0, 0)
@@ -35,7 +37,6 @@ const A = 3
 type rgb = [number, number, number]
 type xy = [number, number]
 
-const { round, floor, ceil, random, min } = Math
 const halfH = round(h / 2)
 const halfW = round(w / 2)
 
@@ -47,10 +48,10 @@ interface Cell {
 
 let all_cells: Cell[] = []
 let edge_cells: Cell[] = []
-let all_colors: rgb[] = []
-let used_colors: number[] = []
+let edge_cell_keys: (string | number)[] = []
+let new_colors: rgb[] = []
 let color_i = 0
-let batch = 64
+let batch = 1
 
 let isStop = true
 let isStopNext = false
@@ -88,88 +89,121 @@ let colorDiff = 16
 let colorDiff2 = colorDiff * 2
 
 function nextColor(): rgb {
-  const color = all_colors[color_i]
-  color_i = (color_i + 1) % all_colors.length
+  if (new_colors.length == 0) {
+    throw new Error('no available colors')
+  }
+  color_i = color_i % new_colors.length
+  const color = new_colors[color_i]
+  color_i++
   return color
 }
 
+function useColor(color: rgb) {
+  let i = new_colors.indexOf(color)
+  new_colors.splice(i, 1)
+}
+
+function addEdgeCell(cell: Cell) {
+  if (cell.color) {
+    return
+  }
+  let key = cell.offset
+  if (edge_cells[key]) {
+    return
+  }
+  edge_cells[cell.offset] = cell
+  edge_cell_keys.push(key)
+}
+
+function addPeerEdgeCell(cell: Cell) {
+  let i = all_cells.indexOf(cell)
+  let peers = [
+    all_cells[i + 1],
+    all_cells[i - 1],
+    all_cells[i + w],
+    all_cells[i - w],
+  ]
+  for (let peer of peers) {
+    if (peer) {
+      addEdgeCell(peer)
+    }
+  }
+}
+
+function nextEdgeCell() {
+  let n = edge_cell_keys.length
+  if (n == 0) {
+    return
+  }
+  // let i = floor(random() * n)
+  // let i = floor(n/2)
+  // let i = floor(n/3*2)
+  let i = n - 256
+  if (i < 0) {
+    i = 0
+  }
+  // let i = n - 1
+  let key = edge_cell_keys[i]
+  let cell = edge_cells[key]
+  delete edge_cells[key]
+  edge_cell_keys.splice(i, 1)
+  return cell
+}
+
 function update(): 'end' | 'continue' {
-  // batch = win.batch || batch
+  const start = Date.now()
   for (let i = 0; i < batch; i++) {
-    if (edge_cells.length == 0) {
+    let cell = nextEdgeCell()
+    if (!cell) {
       console.log('no edge cells')
       return 'end'
     }
 
-    let min_i = -1
     let min_d = Number.MAX_VALUE
     let min_color: rgb
 
-    for (let i = 0; i < edge_cells.length; i++) {
-      if (edge_cells[i].color) {
-        edge_cells.splice(i, 1)
-        i--
-      }
-    }
-
-    const sample = 1000
-    const n_trial = 1
-    // const min_peer = 2
     for (let i = 0; i < n_trial; i++) {
       const color = nextColor()
-      let max_peer = 0
-      const step = ceil(edge_cells.length / sample)
-      for (let i = 0; i < edge_cells.length; i += step) {
-        const cell = edge_cells[i]
-        if (!cell) {
-          break
+      let total_d = 0
+      let peer_count = 0
+      cell.peers.forEach((peer) => {
+        if (!peer.cell.color) {
+          return
         }
-        let total_d = 0
-        let peer_count = 0
-        cell.peers.forEach((peer) => {
-          if (!peer.cell.color) {
-            return
-          }
-          const r = color[0] - peer.cell.color[0]
-          const g = color[1] - peer.cell.color[1]
-          const b = color[2] - peer.cell.color[2]
-          let d = r * r + g * g + b * b
-          d *= peer.d ** 2
-          total_d += d
-          peer_count++
-        })
-        if (peer_count == 0) {
-          console.error('no peer?')
-          return 'end'
-        }
-        const d = total_d / peer_count
-        if (d < min_d && peer_count >= max_peer) {
-          // if (d < min_d && peer_count >= min_peer) {
-          // if (d < min_d) {
-          max_peer = peer_count
-          min_i = i
-          min_d = d
-          min_color = color
-        }
+        const r = color[0] - peer.cell.color[0]
+        const g = color[1] - peer.cell.color[1]
+        const b = color[2] - peer.cell.color[2]
+        let d = r * r + g * g + b * b
+        d *= peer.d ** 2
+        total_d += d
+        peer_count++
+      })
+      if (peer_count == 0) {
+        console.error('no peer?')
+        return 'end'
+      }
+      const d = total_d / peer_count
+      if (d < min_d) {
+        min_d = d
+        min_color = color
       }
     }
 
-    if (min_i == -1) {
-      console.error('min_i not found')
-      return 'end'
-    }
-    const cell = edge_cells[min_i]
-    edge_cells.splice(min_i, 1)
-    cell.peers.forEach((peer) => {
-      if (!peer.cell.color) {
-        edge_cells.push(peer.cell)
-      }
-    })
+    addPeerEdgeCell(cell)
+
     cell.color = min_color
     const offset = cell.offset
     imageData.data[offset + 0] = min_color[0]
     imageData.data[offset + 1] = min_color[1]
     imageData.data[offset + 2] = min_color[2]
+    useColor(min_color)
+  }
+  const end = Date.now()
+  const time = (end - start) / 1000
+  const timePerRun = time / batch
+  batch = ceil(timePerFrame / timePerRun)
+  if (batch > maxBatch) {
+    batch = maxBatch
   }
 }
 
@@ -189,6 +223,8 @@ function loop() {
 function start() {
   let offset = 0
 
+  let all_colors: rgb[] = []
+  let used_colors: number[] = []
   for (let y = 0; y < h; y++) {
     for (let x = 0; x < w; x++) {
       all_cells.push({ offset, peers: [] })
@@ -206,30 +242,31 @@ function start() {
       }
     }
   }
+  new_colors = all_colors
 
-  const range = 1
   for (let y = 0; y < h; y++) {
     for (let x = 0; x < w; x++) {
       const i = y * w + x
       const cell = all_cells[i]
 
-      for (let dy = -range; dy <= range; dy++) {
-        for (let dx = -range; dx <= range; dx++) {
-          if (dx == 0 && dy == 0) {
-            continue
-          }
-          const py = y + dy
-          if (py < 0 || py >= h) {
-            continue
-          }
-          const px = x + dx
-          if (px < 0 || px >= w) {
+      const px_start = max(0, ceil(x - range))
+      const py_start = max(0, ceil(y - range))
+      const px_end = min(w - 1, floor(x + range))
+      const py_end = min(h - 1, floor(y + range))
+      for (let py = py_start; py <= py_end; py++) {
+        for (let px = px_start; px <= px_end; px++) {
+          if (px == x && py == y) {
             continue
           }
           const pi = py * w + px
           const p_cell = all_cells[pi]
-          const d = dx * dx + dy * dy
-          cell.peers.push({ d, cell: p_cell })
+          const dx = x - px
+          const dy = y - py
+          const d2 = dx * dx + dy * dy
+          if (d2 > range2) {
+            continue
+          }
+          cell.peers.push({ d: d2, cell: p_cell })
         }
       }
     }
@@ -239,33 +276,35 @@ function start() {
   isStop = false
   isStopNext = false
 
-  function init_place(i) {
+  function init_place(y: number, x: number) {
+    console.log('init_place', { x, y })
+    let i = y * w + x
     const cell = all_cells[i]
     const offset = cell.offset
     const color = nextColor()
     cell.color = color
-    cell.peers.forEach((peer) => edge_cells.push(peer.cell))
+    addPeerEdgeCell(cell)
     imageData.data[offset + 0] = color[0]
     imageData.data[offset + 1] = color[1]
     imageData.data[offset + 2] = color[2]
   }
 
   // initial paint center cell
-  init_place(halfH * w + halfW)
+  init_place(halfH, halfW)
 
   // to fulfill min_peer
 
   // initial paint center cell (right)
-  init_place(halfH * w + halfW + 1)
+  init_place(halfH, halfW + 1)
 
   // initial paint center cell (left)
-  init_place(halfH * w + halfW - 1)
+  init_place(halfH, halfW - 1)
 
   // initial paint center cell (up)
-  init_place(halfH * w + halfW - w)
+  init_place(halfH - 1, halfW)
 
   // initial paint center cell (down)
-  init_place(halfH * w + halfW + w)
+  init_place(halfH + 1, halfW)
 
   loop()
 }
@@ -285,7 +324,8 @@ Object.assign(window, {
   resume,
   stop,
   stopNext,
-  batch,
+  edge_cells,
+  edge_cell_keys,
   imageData,
   all_cells,
 })
